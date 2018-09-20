d12833f3f30c5f2a57d03f3e10d2d1e13c667899
Amy Berg <aberg2014@gmail.com>
2018-09-20 11:56:35 -0400

    added reducer for products

---
+++ client/store/products.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+// ACTION TYPES
+
+const GET_PRODUCTS = 'GET_PRODUCTS'
+
+// INITIAL STATE
+const initialState = []
+
+// ACTION CREATORS
+
+const getProducts = products => ({
+  type: GET_PRODUCTS,
+  products
+})
+
+// THUNK CREATORS
+
+export const retrieveProducts = () => async dispatch => {
+  try {
+    // make axios request for product data
+    const { data } = await axios.get('https://dev.lillypulitzer.com/s/lillypulitzer-us/dw/shop/v18_3/product_search?expand=availability,images,prices,variations&count=18&refine_1=cgid=just-in&q=&start=0&client_id=7469c353-e112-4902-bf40-ead35df41219')
+    dispatch(getProducts(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+// REDUCER
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return action.products
+    default:
+      return state
+  }
+}
+
+export default reducer
